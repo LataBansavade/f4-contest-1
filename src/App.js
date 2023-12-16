7e1e43b3494368e0abdc1b4ee4adf4e6dbd00faf
lata bansavad <latabansavade24@gmail.com>
2023-12-16 21:25:14 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Email from './Components/Email';
+import Password from './Components/Password';
+
+
+const App=()=> {
+  return (
+    <div className="App">
+      <Email/>
+      <Password/>
+    </div>
+  );
+}
+
+export default App;
